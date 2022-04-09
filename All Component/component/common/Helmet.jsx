587cfdeb088b0aaed9f6eb33c471e5ca245d0f51
Boricha-Nikhil <nikhilboricha777@gmail.com>
2022-04-09 21:46:47 +0530

    package Chnages

--- All Component/component/common/Helmet.jsx
+++ All Component/component/common/Helmet.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import {Helmet} from 'react-helmet'
+import Head from 'next/head'
 
 class PageHelmet extends Component{
     render(){
         return(
             <>
-                <Helmet>
+                <Head>
                     <title>{this.props.pageTitle} || React Multipurpose Template </title>
                     <meta name="description" content="Trydo – Multipurpose React Template is a multi-use React template. It is designed to go well with multi-purpose websites. Trydo React Template will create that various landing Template, Creative Agency, And Corporate Website ." />
-                </Helmet>
+                </Head>
             </>
         )
     }
